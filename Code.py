75548f1f4982df4119db892a1e2209ef7153d97c
Warmisharrow350 <87402431+WarmisharrowPy@users.noreply.github.com>
2021-07-17 18:40:48 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Back
+
+colorama.init(autoreset=True)
+        print(Fore.LIGHTGREEN_EX + Back.BLACK + input("Enter Text: ") + Fore.BLUE + Back.BLACK + input("Enter Text: "))
+        print(Back.BLACK + input("Enter Text: "))
